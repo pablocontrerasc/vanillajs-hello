e543bdb9e97b77ec6ab2928ec0d567e26da2090a
pablocontrerasc <cont.pab@gmail.com>
2022-01-08 17:18:23 +0000

    codigo ok

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const random1 = Math.floor(Math.random() * who.length, what.length);
+  const random2 = Math.floor(Math.random() * what.length, what.length);
+  const random3 = Math.floor(Math.random() * when.length, when.length);
+  let rd1 = who[random1];
+  let rd2 = what[random2];
+  let rd3 = when[random3];
+  let excusa = document.createElement("p");
+  excusa.innerHTML = `${rd1} ${rd2}  ${rd3}`;
+  let container = document.getElementById("container");
+  console.log(container);
+  container.append(excusa);
+
+  //console.log("Hello Rigo from the console!");
 };
